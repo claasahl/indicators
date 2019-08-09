0e4f1d37ec8650b8a6b6615c3a3653f7f30dfc50
Claas Ahlrichs <claasahl@gmail.com>
2019-08-09 16:31:21 +0200

    track documentation

---
+++ docs/assets/js/search.js
@@ -0,0 +1,352 @@
+var typedoc = typedoc || {};
+typedoc.search = typedoc.search || {};
+typedoc.search.data = {
+  kinds: {
+    "1": "External module",
+    "64": "Function",
+    "256": "Interface",
+    "1024": "Property",
+    "4194304": "Type alias"
+  },
+  rows: [
+    {
+      id: 0,
+      kind: 1,
+      name: '"candle"',
+      url: "modules/_candle_.html",
+      classes: "tsd-kind-external-module"
+    },
+    {
+      id: 1,
+      kind: 256,
+      name: "Candle",
+      url: "interfaces/_candle_.candle.html",
+      classes: "tsd-kind-interface tsd-parent-kind-external-module",
+      parent: '"candle"'
+    },
+    {
+      id: 2,
+      kind: 1024,
+      name: "timestamp",
+      url: "interfaces/_candle_.candle.html#timestamp",
+      classes: "tsd-kind-property tsd-parent-kind-interface",
+      parent: '"candle".Candle'
+    },
+    {
+      id: 3,
+      kind: 1024,
+      name: "period",
+      url: "interfaces/_candle_.candle.html#period",
+      classes: "tsd-kind-property tsd-parent-kind-interface",
+      parent: '"candle".Candle'
+    },
+    {
+      id: 4,
+      kind: 1024,
+      name: "volume",
+      url: "interfaces/_candle_.candle.html#volume",
+      classes: "tsd-kind-property tsd-parent-kind-interface",
+      parent: '"candle".Candle'
+    },
+    {
+      id: 5,
+      kind: 1024,
+      name: "open",
+      url: "interfaces/_candle_.candle.html#open",
+      classes: "tsd-kind-property tsd-parent-kind-interface",
+      parent: '"candle".Candle'
+    },
+    {
+      id: 6,
+      kind: 1024,
+      name: "high",
+      url: "interfaces/_candle_.candle.html#high",
+      classes: "tsd-kind-property tsd-parent-kind-interface",
+      parent: '"candle".Candle'
+    },
+    {
+      id: 7,
+      kind: 1024,
+      name: "low",
+      url: "interfaces/_candle_.candle.html#low",
+      classes: "tsd-kind-property tsd-parent-kind-interface",
+      parent: '"candle".Candle'
+    },
+    {
+      id: 8,
+      kind: 1024,
+      name: "close",
+      url: "interfaces/_candle_.candle.html#close",
+      classes: "tsd-kind-property tsd-parent-kind-interface",
+      parent: '"candle".Candle'
+    },
+    {
+      id: 9,
+      kind: 64,
+      name: "upper",
+      url: "modules/_candle_.html#upper",
+      classes: "tsd-kind-function tsd-parent-kind-external-module",
+      parent: '"candle"'
+    },
+    {
+      id: 10,
+      kind: 64,
+      name: "lower",
+      url: "modules/_candle_.html#lower",
+      classes: "tsd-kind-function tsd-parent-kind-external-module",
+      parent: '"candle"'
+    },
+    {
+      id: 11,
+      kind: 64,
+      name: "body",
+      url: "modules/_candle_.html#body",
+      classes: "tsd-kind-function tsd-parent-kind-external-module",
+      parent: '"candle"'
+    },
+    {
+      id: 12,
+      kind: 64,
+      name: "hair",
+      url: "modules/_candle_.html#hair",
+      classes: "tsd-kind-function tsd-parent-kind-external-module",
+      parent: '"candle"'
+    },
+    {
+      id: 13,
+      kind: 64,
+      name: "tail",
+      url: "modules/_candle_.html#tail",
+      classes: "tsd-kind-function tsd-parent-kind-external-module",
+      parent: '"candle"'
+    },
+    {
+      id: 14,
+      kind: 64,
+      name: "bullish",
+      url: "modules/_candle_.html#bullish",
+      classes: "tsd-kind-function tsd-parent-kind-external-module",
+      parent: '"candle"'
+    },
+    {
+      id: 15,
+      kind: 64,
+      name: "bearish",
+      url: "modules/_candle_.html#bearish",
+      classes: "tsd-kind-function tsd-parent-kind-external-module",
+      parent: '"candle"'
+    },
+    {
+      id: 16,
+      kind: 1,
+      name: '"trend"',
+      url: "modules/_trend_.html",
+      classes: "tsd-kind-external-module"
+    },
+    {
+      id: 17,
+      kind: 4194304,
+      name: "Trend",
+      url: "modules/_trend_.html#trend",
+      classes: "tsd-kind-type-alias tsd-parent-kind-external-module",
+      parent: '"trend"'
+    },
+    {
+      id: 18,
+      kind: 64,
+      name: "up",
+      url: "modules/_trend_.html#up",
+      classes: "tsd-kind-function tsd-parent-kind-external-module",
+      parent: '"trend"'
+    },
+    {
+      id: 19,
+      kind: 64,
+      name: "down",
+      url: "modules/_trend_.html#down",
+      classes: "tsd-kind-function tsd-parent-kind-external-module",
+      parent: '"trend"'
+    },
+    {
+      id: 20,
+      kind: 1,
+      name: '"candles/beltHold"',
+      url: "modules/_candles_belthold_.html",
+      classes: "tsd-kind-external-module"
+    },
+    {
+      id: 21,
+      kind: 64,
+      name: "isBeltHold",
+      url: "modules/_candles_belthold_.html#isbelthold",
+      classes: "tsd-kind-function tsd-parent-kind-external-module",
+      parent: '"candles/beltHold"'
+    },
+    {
+      id: 22,
+      kind: 1,
+      name: '"candles/hammer"',
+      url: "modules/_candles_hammer_.html",
+      classes: "tsd-kind-external-module"
+    },
+    {
+      id: 23,
+      kind: 64,
+      name: "isHammer",
+      url: "modules/_candles_hammer_.html#ishammer",
+      classes: "tsd-kind-function tsd-parent-kind-external-module",
+      parent: '"candles/hammer"'
+    },
+    {
+      id: 24,
+      kind: 1,
+      name: '"candles/hangingMan"',
+      url: "modules/_candles_hangingman_.html",
+      classes: "tsd-kind-external-module"
+    },
+    {
+      id: 25,
+      kind: 64,
+      name: "isHangingMan",
+      url: "modules/_candles_hangingman_.html#ishangingman",
+      classes: "tsd-kind-function tsd-parent-kind-external-module",
+      parent: '"candles/hangingMan"'
+    },
+    {
+      id: 26,
+      kind: 1,
+      name: '"candles/engulfing"',
+      url: "modules/_candles_engulfing_.html",
+      classes: "tsd-kind-external-module"
+    },
+    {
+      id: 27,
+      kind: 64,
+      name: "isEngulfing",
+      url: "modules/_candles_engulfing_.html#isengulfing",
+      classes: "tsd-kind-function tsd-parent-kind-external-module",
+      parent: '"candles/engulfing"'
+    },
+    {
+      id: 28,
+      kind: 64,
+      name: "engulfed",
+      url: "modules/_candles_engulfing_.html#engulfed",
+      classes:
+        "tsd-kind-function tsd-parent-kind-external-module tsd-is-not-exported",
+      parent: '"candles/engulfing"'
+    },
+    {
+      id: 29,
+      kind: 1,
+      name: '"candles/harami"',
+      url: "modules/_candles_harami_.html",
+      classes: "tsd-kind-external-module"
+    },
+    {
+      id: 30,
+      kind: 64,
+      name: "isHarami",
+      url: "modules/_candles_harami_.html#isharami",
+      classes: "tsd-kind-function tsd-parent-kind-external-module",
+      parent: '"candles/harami"'
+    },
+    {
+      id: 31,
+      kind: 64,
+      name: "engulfed",
+      url: "modules/_candles_harami_.html#engulfed",
+      classes:
+        "tsd-kind-function tsd-parent-kind-external-module tsd-is-not-exported",
+      parent: '"candles/harami"'
+    },
+    {
+      id: 32,
+      kind: 1,
+      name: '"candles/index"',
+      url: "modules/_candles_index_.html",
+      classes: "tsd-kind-external-module"
+    },
+    {
+      id: 33,
+      kind: 1,
+      name: '"indicators/simpleMovingAverage"',
+      url: "modules/_indicators_simplemovingaverage_.html",
+      classes: "tsd-kind-external-module"
+    },
+    {
+      id: 34,
+      kind: 64,
+      name: "simpleMovingAverage",
+      url: "modules/_indicators_simplemovingaverage_.html#simplemovingaverage",
+      classes: "tsd-kind-function tsd-parent-kind-external-module",
+      parent: '"indicators/simpleMovingAverage"'
+    },
+    {
+      id: 35,
+      kind: 1,
+      name: '"indicators/index"',
+      url: "modules/_indicators_index_.html",
+      classes: "tsd-kind-external-module"
+    },
+    {
+      id: 36,
+      kind: 1,
+      name: '"index"',
+      url: "modules/_index_.html",
+      classes: "tsd-kind-external-module"
+    },
+    {
+      id: 37,
+      kind: 1,
+      name: '"samples"',
+      url: "modules/_samples_.html",
+      classes: "tsd-kind-external-module"
+    },
+    {
+      id: 38,
+      kind: 256,
+      name: "Sample",
+      url: "interfaces/_samples_.sample.html",
+      classes:
+        "tsd-kind-interface tsd-parent-kind-external-module tsd-is-not-exported",
+      parent: '"samples"'
+    },
+    {
+      id: 39,
+      kind: 1024,
+      name: "trend",
+      url: "interfaces/_samples_.sample.html#trend",
+      classes:
+        "tsd-kind-property tsd-parent-kind-interface tsd-is-not-exported",
+      parent: '"samples".Sample'
+    },
+    {
+      id: 40,
+      kind: 1024,
+      name: "candles",
+      url: "interfaces/_samples_.sample.html#candles",
+      classes:
+        "tsd-kind-property tsd-parent-kind-interface tsd-is-not-exported",
+      parent: '"samples".Sample'
+    },
+    {
+      id: 41,
+      kind: 64,
+      name: "drawCandles",
+      url: "modules/_samples_.html#drawcandles",
+      classes:
+        "tsd-kind-function tsd-parent-kind-external-module tsd-is-not-exported",
+      parent: '"samples"'
+    },
+    {
+      id: 42,
+      kind: 64,
+      name: "generateSamples",
+      url: "modules/_samples_.html#generatesamples",
+      classes:
+        "tsd-kind-function tsd-parent-kind-external-module tsd-is-not-exported",
+      parent: '"samples"'
+    }
+  ]
+};
