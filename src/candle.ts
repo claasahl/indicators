90e0a865cc4fc7c74bcb0335f7d5dde62817fcf7
Claas Ahlrichs <claasahl@gmail.com>
2020-03-22 09:55:19 +0100

    updated signatures of helper functions

--- src/candle.ts
+++ src/candle.ts
@@ -8,34 +8,34 @@ export interface Candle {
   close: number;
 }
 
-export function upper(candle: Candle): number {
+export function upper(candle: Pick<Candle, "open" | "close">): number {
   return Math.max(candle.open, candle.close);
 }
 
-export function lower(candle: Candle): number {
+export function lower(candle: Pick<Candle, "open" | "close">): number {
   return Math.min(candle.open, candle.close);
 }
 
-export function body(candle: Candle): number {
+export function body(candle: Pick<Candle, "open" | "close">): number {
   return Math.abs(candle.open - candle.close);
 }
 
-export function range(candle: Candle): number {
+export function range(candle: Pick<Candle, "high" | "low">): number {
   return candle.high - candle.low;
 }
 
-export function hair(candle: Candle): number {
+export function hair(candle: Pick<Candle, "open" | "close" | "high">): number {
   return candle.high - upper(candle);
 }
 
-export function tail(candle: Candle): number {
+export function tail(candle: Pick<Candle, "open" | "close" | "low">): number {
   return lower(candle) - candle.low;
 }
 
-export function bullish(candle: Candle): boolean {
+export function bullish(candle: Pick<Candle, "open" | "close">): boolean {
   return candle.open < candle.close;
 }
 
-export function bearish(candle: Candle): boolean {
+export function bearish(candle: Pick<Candle, "open" | "close">): boolean {
   return candle.open > candle.close;
 }
